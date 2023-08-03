1310c99cbc65e60abfe5648f34ae8338c8f752db
rmit-kent-go <s3955624@student.rmit.edu.au>
2023-08-03 12:02:40 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; /* DELETE THIS */
 import './App.css';
 import Home from "./pages/Home";
+
+
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
