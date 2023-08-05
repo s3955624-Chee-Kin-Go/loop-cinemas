e8e95dd9fcf70d9ea0555f9bec055dcaffef9ddc
rmit-kent-go <s3955624@student.rmit.edu.au>
2023-08-05 19:13:16 +1000

    added About Us section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,6 +35,14 @@ function Home() {
         </div>
       </div>
     </section>
+
+    <section className="about-us-section">
+      <h1>ABOUT US</h1>
+      <p>Welcome to Loop Cinemas, where movie magic comes to life. 
+        Experience immersive cinema with state-of-the-art technology and a curated selection of blockbusters, classics, and indie gems. 
+        Our commitment to excellence ensures unforgettable moments on the big screen. 
+        Join us at Loop Cinemas for a reel adventure unlike any other.</p>
+    </section>
     <Footer />
     </>
   );
