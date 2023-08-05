c8e652b226cdc87c743f6bdd2779ef62dc7bf91f
rmit-kent-go <s3955624@student.rmit.edu.au>
2023-08-05 19:26:45 +1000

    added cinema image in About Us

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,11 +37,17 @@ function Home() {
     </section>
 
     <section className="about-us-section">
-      <h1>ABOUT US</h1>
-      <p>Welcome to Loop Cinemas, where movie magic comes to life. 
-        Experience immersive cinema with state-of-the-art technology and a curated selection of blockbusters, classics, and indie gems. 
-        Our commitment to excellence ensures unforgettable moments on the big screen. 
-        Join us at Loop Cinemas for a reel adventure unlike any other.</p>
+      <div className='about-us-text'>
+        <h1>ABOUT US</h1>
+        <p>Welcome to Loop Cinemas, where movie magic comes to life.</p>
+        <p>Experience immersive cinema with state-of-the-art technology and a curated selection of blockbusters, classics, and indie gems.</p>
+        <p>Our commitment to excellence ensures unforgettable moments on the big screen.</p>
+        <p>Join us at Loop Cinemas for a reel adventure unlike any other.</p>
+      </div>
+      <div className='about-us-img'>
+        <img src='https://cdn.eventcinemas.com.au/cdn/resources/experience/vmax/about/images/840abc97-1d62-436e-b088-01611daef1cf.jpg'/>
+      </div>
+      
     </section>
     <Footer />
     </>
