ee406f7763a42fee7cb88d6045836556ccac5d6f
Evelyn Lie <s3951140@student.rmit.edu.au>
2023-08-06 13:32:06 +1000

    Implement Navbar

--- src/fragments/Navbar.js
+++ src/fragments/Navbar.js
@@ -1,50 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css';
 
-function Navbar(props) {
+function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <Link className="navbar-brand" to="/">React App</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            {props.username !== null &&
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/profile">My Profile</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/forum">Forum</Link>
-                </li>
-              </>
-            }
-          </ul>
-          <ul className="navbar-nav">
-            {props.username === null ?
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
-              </li>
-              :
-              <>
-                <li className="nav-item">
-                  <span className="nav-link text-light">Welcome, {props.username}</span>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login" onClick={props.logoutUser}>Logout</Link>
-                </li>
-              </>
-            }
-          </ul>
-        </div>
+    <>
+      <div className="navbar">
+          <Link to="/">Logo</Link>
+          <Link className="nav-link" to="/login">Login</Link>
       </div>
-    </nav>
+    </>
   );
 }
 
