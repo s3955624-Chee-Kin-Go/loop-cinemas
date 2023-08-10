c035bae78ed9140642d06466cd52dd01b3597fce
Evelyn Lie <s3951140@student.rmit.edu.au>
2023-08-10 11:58:01 +1000

    Style profile page

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,13 +1,16 @@
 import React from "react";
+import './pagesCSS/MyProfile.css'
 
 function MyProfile(props) {
   return (
-    <div>
-      <h1 className="display-4">My Profile</h1>
-      <h4><strong>Hello {props.username}!</strong></h4>
-      {props.email !== null && <h4><strong>Email: {props.email}</strong></h4>}
-      {props.password !== null && <h4><strong>Password: {props.password}</strong></h4>}
-    </div>
+    <>
+      <div className="section-title">
+        <h1>My Profile</h1>
+        Hello, {props.username}!
+        {props.email !== null && <p>Email: {props.email} <br></br> 
+        Joined: {props.joined}</p>}
+      </div>
+    </>
   );
 }
 
