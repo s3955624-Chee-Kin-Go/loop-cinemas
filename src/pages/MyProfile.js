31c1e5bdbc504ec63fb76ea7fee0653c855d449a
Evelyn Lie <s3951140@student.rmit.edu.au>
2023-08-10 15:26:17 +1000

    Create edit and delete profile button

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,14 +1,47 @@
 import React from "react";
+import { removeUser } from "../data/repository";
+import { useNavigate } from "react-router-dom";
 import './pagesCSS/MyProfile.css'
+import {
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
 function MyProfile(props) {
+  const navigate = useNavigate();
+  // FIX THIS: Implement remove user functionality
+  const handleEditUser = (event) => {
+    event.preventDefault();
+  };
+
+  // FIX THIS: Implement remove user functionality
+  const handleRemoveUser = (event) => {
+    event.preventDefault();
+    const confirmDelete = window.confirm("Are you sure you want to delete your profile?");
+    if (confirmDelete) {
+      // Remove user from localStorage
+      
+      // Navigate to the home page.
+      navigate("/");
+    }
+  };
+
   return (
     <>
       <div className="profile">
-        <h2>My Profile</h2>
-        Hello, {props.username}!
-        {props.email !== null && <p>Email: {props.email} <br></br> 
-        Joined: {props.signupDate}</p>} {/* Add function to save user's sign up date*/}
+        <div className="profile-header">
+          <h2>My Profile</h2>
+          <MDBBtn outline color="light" floating href='' role='button' className='edit-icon' onClick={handleEditUser}>
+            <MDBIcon far icon="edit" style={{fontSize:'1rem'}}/>
+          </MDBBtn>
+          <MDBBtn outline color="light" floating href='' role='button' className='edit-icon' onClick={handleRemoveUser}>
+             <MDBIcon far icon="trash-alt" style={{fontSize:'1rem'}}/>
+          </MDBBtn>
+        </div>
+        <div className="profile-info">
+          Hello, {props.username}!
+          {props.email !== null && <p>Email: {props.email} <br /> Joined: {props.signupDate}</p>}
+        </div>
       </div>
     </>
   );
